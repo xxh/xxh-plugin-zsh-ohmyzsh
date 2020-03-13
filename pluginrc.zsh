ae8ecbfab5dc327cefa8881edf02853646107d6b
anki-code <anki-code>
2020-03-13 23:51:30 +0300

    0.5.6

--- pluginrc.zsh
+++ pluginrc.zsh
@@ -1,38 +1,41 @@
+plugin_env_name='XXH_PLUGIN_ZSH_OHMYZSH'
 CURR_DIR="$(cd "$(dirname "$0")" && pwd)"
 
 export ZSH="$CURR_DIR/ohmyzsh"
 
-if [ $ZSH_THEME ]; then
+# Search EXE environment variables for plugin and execute
+prefix="$plugin_env_name"_EXE_
+for l in `env | grep $prefix`; do
+  if [[ $XXH_VERBOSE == '2' ]]; then
+    echo $plugin_env_name: found $l
+  fi
+  data="$( cut -d '=' -f 2- <<< "$l" )";
+  code=`echo $data | base64 -d`
+  if [[ $XXH_VERBOSE == '2' ]]; then
+    echo Run: $code
+  fi
+  eval $code
+done
+
+if [[ -v ZSH_THEME ]]; then
   if [[ $XXH_VERBOSE == '2' ]]; then
     echo Found ZSH_THEME=$ZSH_THEME
   fi
-  if [[ $ZSH_THEME == *"("* ]]; then
-    echo "Something went wrong while running zsh on host:"
-    echo $check_result
-  else
-    echo $check_result > .entrypoint-check-done
-  fi
-
-elif [ $XXH_ZSH_PLUGIN_OHMYZSH_ZSH_THEME ]; then
-  export ZSH_THEME="$XXH_ZSH_PLUGIN_OHMYZSH_ZSH_THEME"
 else
+  if [[ $XXH_VERBOSE == '2' ]]; then
+    echo Set default ZSH_THEME=agnoster
+  fi
   export ZSH_THEME="agnoster"
 fi
 
-if [ $plugins ]; then
+if [[ -v plugins ]]; then
   if [[ $XXH_VERBOSE == '2' ]]; then
     echo Found plugins=$plugins
   fi
-  if [[ $plugins == *"("* ]]; then
-    p=`echo -n $plugins | sed 's/\(^(\|)$\)//g'`
-    IFS=' ' read -r -A plugins <<< "$p"
-    export plugins
-  fi
-elif [ $XXH_ZSH_PLUGIN_OHMYZSH_PLUGINS ]; then
-  p=`echo -n $XXH_ZSH_PLUGIN_OHMYZSH_PLUGINS | sed 's/\(^(\|)$\)//g'`
-  IFS=' ' read -r -A plugins <<< "$p"
-  export plugins
 else
+  if [[ $XXH_VERBOSE == '2' ]]; then
+    echo Set default plugins=git
+  fi
   export plugins=(git)
 fi
 
