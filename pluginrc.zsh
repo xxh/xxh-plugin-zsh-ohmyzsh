651430dfb4f8621c2fd26b8fb128c3d58039516c
anki-code <anki-code>
2020-03-13 14:46:33 +0300

    0.5.3

--- pluginrc.zsh
+++ pluginrc.zsh
@@ -2,13 +2,21 @@ CURR_DIR="$(cd "$(dirname "$0")" && pwd)"
 
 export ZSH="$CURR_DIR/ohmyzsh"
 
-if [ $XXH_ZSH_PLUGIN_OHMYZSH_ZSH_THEME ]; then
+if [ $ZSH_THEME ]; then
+  if [[ $XXH_VERBOSE == '2' ]]; then
+    echo Found ZSH_THEME=$ZSH_THEME
+  fi
+elif [ $XXH_ZSH_PLUGIN_OHMYZSH_ZSH_THEME ]; then
   export ZSH_THEME="$XXH_ZSH_PLUGIN_OHMYZSH_ZSH_THEME"
 else
   export ZSH_THEME="agnoster"
 fi
 
-if [ $XXH_ZSH_PLUGIN_OHMYZSH_PLUGINS ]; then
+if [ $plugins ]; then
+  if [[ $XXH_VERBOSE == '2' ]]; then
+    echo Found plugins=$plugins
+  fi
+elif [ $XXH_ZSH_PLUGIN_OHMYZSH_PLUGINS ]; then
   p=`echo -n $XXH_ZSH_PLUGIN_OHMYZSH_PLUGINS | sed 's/\(^(\|)$\)//g'`
   IFS=' ' read -r -A plugins <<< "$p"
   export plugins
